bccaa602a6ed228aa144c03c495c3bf708a6e91a
Franco Naghetini <franconaghetini@gmail.com>
2022-08-12 13:59:30 -0300

    Finish update_scripts first version

--- app.py
+++ app.py
@@ -83,8 +83,7 @@ def update_scripts():
             table = pd.read_csv(file, sep=',', header=0, dtype=str)
 
             # Colunas
-            cols_list = [f'[{col}],' for col in table.columns]
-            cols_str = ''.join(map(str, cols_list))[:-1]
+            cols_list = list(table.columns)
 
             # Criação do 1° script SQL
             n_script = 1
@@ -100,27 +99,31 @@ def update_scripts():
                     script = open(f"{file[:-4]}_UPDATE_pt0{str(n_script)}.sql", 'w+')
 
                     values_list = [f"'{value}'," if not pd.isna(value) else "NULL," for value in table.iloc[i, :]]
-                    values_str = ''.join(map(str, values_list))[:-1]
-                    row = f"UPDATE {input_table} ({cols_str}) values ({values_str});\n"
+                    cols_values_list = [f"{col} = {value}" for col, value in zip(cols_list, values_list)]
+                    cols_values_str = ''.join(map(str, cols_values_list))[:-1]
+
+                    row = f"UPDATE {input_table} SET {cols_values_str} WHERE sample_number = '{table.loc[i,'sample_number']}';\n"
                     script.write(row)
 
                 elif i == len(table):
                     values_list = [f"'{value}'," if not pd.isna(value) else "NULL," for value in table.iloc[i, :]]
-                    values_str = ''.join(map(str, values_list))[:-1]
+                    cols_values_list = [f"{col} = {value}" for col, value in zip(cols_list, values_list)]
+                    cols_values_str = ''.join(map(str, cols_values_list))[:-1]
 
-                    row = f"UPDATE {input_table} ({cols_str}) values ({values_str});\n"
+                    row = f"UPDATE {input_table} SET {cols_values_str} WHERE sample_number = '{table.loc[i,'sample_number']}';\n"
                     script.write(row)
 
                     script.close()
 
                 else:
                     values_list = [f"'{value}'," if not pd.isna(value) else "NULL," for value in table.iloc[i, :]]
-                    values_str = ''.join(map(str, values_list))[:-1]
+                    cols_values_list = [f"{col} = {value}" for col, value in zip(cols_list, values_list)]
+                    cols_values_str = ''.join(map(str, cols_values_list))[:-1]
 
-                    row = f"UPDATE {input_table} ({cols_str}) values ({values_str});\n"
+                    row = f"UPDATE {input_table} SET {cols_values_str} WHERE sample_number = '{table.loc[i, 'sample_number']}';\n"
                     script.write(row)
 
-    messagebox.showinfo('Processo Concluído', f'INSERT script(s) gerado(s) com sucesso na pasta {folder_path}.')
+    messagebox.showinfo('Processo Concluído', f'UPDATE script(s) gerado(s) com sucesso na pasta {folder_path}.')
 
 
 def insert_data():
@@ -139,31 +142,32 @@ root.wm_iconbitmap('datamine.ico')
 # Título
 root.title("Datamine GDMS")
 # Dimensões da tabela
-root.geometry("380x430")
+root.geometry("500x430")
 # Configuração de background
 root.configure(background='white')
 
 # Widgets
 txt_title1 = Label(root, text="Geração de Scripts SQL", bg='white', fg='black', font="lucida 12 bold")
-txt_dir1 = Label(root, text="Diretório:", bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
+txt_dir1 = Label(root, text="Diretório:", width=30, bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
 tbx_dir1 = Text(root, height=1, width=30, bg='light yellow')
-txt_table = Label(root, text="Nome da Tabela:", bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
+txt_table = Label(root, text="Nome da Tabela:", width=30, bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
 tbx_table = Text(root, height=1, width=30, bg='light yellow')
-btn_generate_scripts = Button(root, text="Gerar Scripts", width=20, command=lambda: insert_scripts())
+btn_insert_scripts = Button(root, text="Gerar INSERT Scripts", width=20, justify=RIGHT, command=lambda: insert_scripts())
+btn_update_scripts = Button(root, text="Gerar UPDATE Scripts", width=20, justify=CENTER, command=lambda: update_scripts())
 
 txt_title2 = Label(root, text="Inserção dos Dados no Banco", bg='white', fg='black', font="lucida 12 bold")
-txt_dir2 = Label(root, text="Diretório:", bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
+txt_dir2 = Label(root, text="Diretório:", width=30, bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
 tbx_dir2 = Text(root, height=1, width=30, bg='light yellow')
-txt_server = Label(root, text="Servidor:", bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
+txt_server = Label(root, text="Servidor:", width=30, bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
 tbx_server = Text(root, height=1, width=30, bg='light yellow')
-txt_db = Label(root, text="Banco de Dados:", bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
+txt_db = Label(root, text="Banco de Dados:", width=30, bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
 tbx_db = Text(root, height=1, width=30, bg='light yellow')
-txt_user = Label(root, text="Usuário:", bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
+txt_user = Label(root, text="Usuário:", width=30, bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
 tbx_user = Text(root, height=1, width=30, bg='light yellow')
-txt_pwd = Label(root, text="Senha:", bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
+txt_pwd = Label(root, text="Senha:", width=30, bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
 tbx_pwd = Text(root, height=1, width=30, bg='light yellow')
 btn_insert_data = Button(root, text="Inserir Dados", width=20, command=lambda: insert_data())
-txt_version = Label(root, text="v0.0.2", bg='white', fg='black', justify=RIGHT, anchor='e')
+txt_version = Label(root, text="v0.0.3", bg='white', fg='black', justify=RIGHT, anchor='e')
 
 # Associando widgets à janela principal
 txt_title1.grid(row=0, column=0, columnspan=2, pady=10)
@@ -171,7 +175,8 @@ txt_dir1.grid(row=1, column=0, pady=5, sticky=W)
 tbx_dir1.grid(row=1, column=1)
 txt_table.grid(row=2, column=0, pady=5, sticky=W)
 tbx_table.grid(row=2, column=1)
-btn_generate_scripts.grid(row=3, column=0, columnspan=2, pady=10)
+btn_insert_scripts.grid(row=3, column=0, pady=10)
+btn_update_scripts.grid(row=3, column=1, pady=10)
 
 txt_title2.grid(row=4, column=0, columnspan=2, pady=10)
 txt_dir2.grid(row=5, column=0, pady=5, sticky=W)
