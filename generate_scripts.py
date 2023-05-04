9a8599524daca32c970aa2ee3616996cb4fbc8d8
Franco Naghetini <franconaghetini@gmail.com>
2023-05-04 09:02:37 -0300

    Update version from 0.0.6 to 0.0.7

--- generate_scripts.py
+++ generate_scripts.py
@@ -45,7 +45,7 @@ tbx_pwd = Text(root, height=1, width=30, bg='white')
 
 btn_insert_data = Button(root, text="Executar Script(s)", width=20, justify=CENTER, cursor='hand2',
                          command=lambda: insert_data_into_db(driver_var, tbx_server, tbx_db, tbx_user, tbx_pwd))
-txt_version = Label(root, text="v0.0.6", bg='white', fg='black', justify=RIGHT, anchor='e')
+txt_version = Label(root, text="v0.0.7", bg='white', fg='black', justify=RIGHT, anchor='e')
 
 # Posição dos widgets - Geração de Scripts SQL
 txt_title1.grid(row=0, column=0, columnspan=2, pady=10)
