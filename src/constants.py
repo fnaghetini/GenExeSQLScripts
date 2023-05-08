7065c1d3c23b8b80708a05adf33389ebcc386c5f
Franco Naghetini <franconaghetini@gmail.com>
2023-05-08 14:27:18 -0300

    Convert table keys name to lower case

--- src/constants.py
+++ src/constants.py
@@ -2,16 +2,16 @@
 DATE_CONVENTION = 103
 
 # Relação tabela -> chave primária
-TABLE_KEY_RELATIONSHIP = {'DRILL_HOLE': ['HOLE_NUMBER'],
-                          'DRILL_HOLE_COORDINATE': ['HOLE_NUMBER'],
-                          'DRILL_HOLE_DIRECTION': ['HOLE_NUMBER', 'DEPTH'],
-                          'HOLE_ASSAY_SAMPLE': ['SAMPLE_NUMBER'],
-                          'HOLE_INTERVAL': ['HOLE_NUMBER', 'depth_from'],
-                          'UDEF_CORE_RECOVERY': ['HOLE_NUMBER', 'depth_from'],
-                          'UDEF_GEOLOGY': ['HOLE_NUMBER', 'depth_from'],
-                          'UDEF_DHL_GEOLOGY': ['HOLE_NUMBER', 'depth_from'],
-                          'UDEF_DHL_GEOTECNIC': ['HOLE_NUMBER', 'depth_from'],
-                          'UDEF_DHL_DENSITY': ['HOLE_NUMBER', 'depth_from'],
+TABLE_KEY_RELATIONSHIP = {'DRILL_HOLE': ['hole_number'],
+                          'DRILL_HOLE_COORDINATE': ['hole_number'],
+                          'DRILL_HOLE_DIRECTION': ['hole_number', 'depth'],
+                          'HOLE_ASSAY_SAMPLE': ['sample_number'],
+                          'HOLE_INTERVAL': ['hole_number', 'depth_from'],
+                          'UDEF_CORE_RECOVERY': ['hole_number', 'depth_from'],
+                          'UDEF_GEOLOGY': ['hole_number', 'depth_from'],
+                          'UDEF_DHL_GEOLOGY': ['hole_number', 'depth_from'],
+                          'UDEF_DHL_GEOTECNIC': ['hole_number', 'depth_from'],
+                          'UDEF_DHL_DENSITY': ['hole_number', 'depth_from'],
                           'UDEF_DHL_DENSITY_QAQC': ['sample_number']}
 
 # Limite de linhas para scripts
