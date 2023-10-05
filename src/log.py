5b251d67b9b3cd714710699f1122d0b0520fdba1
Franco Naghetini <franconaghetini@gmail.com>
2023-10-05 13:39:16 -0300

    Translate log module messages to English

--- src/log.py
+++ src/log.py
@@ -9,15 +9,15 @@ class Log:
         self.start_time = datetime.now()
 
     def display_date(self):
-        execution_date = f'Data: {self.execution_date}\n'
+        execution_date = f'Date: {self.execution_date}\n'
         self.logfile.write(execution_date)
         self.logfile.flush()
 
     def show_error_message(self, error_message):
-        full_error_message = f'ERRO ({datetime.now().strftime("%H:%M:%S")}): {error_message}!\n'
+        full_error_message = f'ERROR ({datetime.now().strftime("%H:%M:%S")}): {error_message}!\n'
         self.logfile.write(full_error_message)
         self.logfile.flush()
-        messagebox.showerror('Erro', full_error_message)
+        messagebox.showerror('Error', full_error_message)
         raise Exception(full_error_message)
 
     def show_progress_message(self, progress_message, display_time=False):
@@ -35,23 +35,23 @@ class Log:
     def display_execution_time(self, new_start_time=None, full_exec_time=False):
         if new_start_time is not None:
             execution_time = datetime.now() - new_start_time
-            self.logfile.write(f'Tempo de execução: {execution_time}\n')
+            self.logfile.write(f'Execution time: {execution_time}\n')
             self.logfile.flush()
         else:
             execution_time = datetime.now() - self.start_time
             if full_exec_time:
-                self.logfile.write(f'\nTEMPO DE EXECUÇÃO TOTAL: {execution_time}\n\n')
+                self.logfile.write(f'\nTOTAL EXECUTION TIME: {execution_time}\n\n')
             else:
-                self.logfile.write(f'Tempo de execução: {execution_time}\n')
+                self.logfile.write(f'Execution time: {execution_time}\n')
             self.logfile.flush()
 
     def show_completion_message(self, completion_message, write_log=True, show_messagebox=True):
         if write_log:
-            full_completion_message = f'\n\nPROCESSO CONCLUÍDO! {completion_message}'
+            full_completion_message = f'\n\nPROCESS FINISHED! {completion_message}'
             self.logfile.write(full_completion_message)
             self.logfile.flush()
         if show_messagebox:
-            messagebox.showinfo('Processo Concluído', completion_message)
+            messagebox.showinfo('Process Finished', completion_message)
 
     def close_logfile(self):
         if self.logfile:
